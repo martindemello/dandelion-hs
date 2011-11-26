6660151c1df2d523433cf43b944873549fb2ffe5
Martin DeMello <martindemello@gmail.com>
2011-11-25 21:56:00 -0800

    we get striping

--- dandelion.hs
+++ dandelion.hs
@@ -6,15 +6,28 @@ import qualified Graphics.UI.Gtk.Abstract.Widget as W
 import qualified Graphics.Rendering.Cairo as C
 import Control.Monad.Trans (liftIO)
 
-type Editor = IORef [Entry]
+type Editor = IORef [PairBox]
 
 newEditor :: IO Editor
 newEditor = newIORef []
 
 
+data PairBox = Pair VBox
+
 data EditorView = EditorView { displayBox :: VBox
                              , currentLine :: Int
                              }
+
+makePair :: String -> IO PairBox
+makePair s = do
+  box <- vBoxNew False 0
+  label <- labelNew (Just s)
+  entry <- makeEntry ""
+  addToBox box label
+  addToBox box entry
+  return $ Pair box
+
+viewBox (Pair v) = v
 
 -- new textfield
 makeEntry :: String -> IO Entry
@@ -25,52 +38,54 @@ makeEntry str = do e <- entryNew
 -- add widget to box with default params
 addToBox box widget = boxPackStart box widget PackNatural 0
 
+addPairToBox box v = addToBox box (viewBox v)
+
 -- editor functions
-addToEditor :: Editor -> Entry -> IO Editor
+addToEditor :: Editor -> PairBox -> IO Editor
 addToEditor ed w = do
   es <- readIORef ed
   writeIORef ed (w : es)
   return ed
 
-removeFromEditor :: Editor -> IO Entry
+removeFromEditor :: Editor -> IO PairBox
 removeFromEditor ed = do
   es <- readIORef ed
   e <- return $ head es
   writeIORef ed (tail es)
   return e
 
-addNewLine :: Editor -> IO Entry
-addNewLine ed = do
-  l <- makeEntry ""
+addNewLine :: Editor -> String -> IO PairBox
+addNewLine ed s = do
+  l <- makePair s
   addToEditor ed l
   return l
 
 addLines :: Editor -> Int -> IO Editor
 addLines ed i = do
-  sequence_ (map (\x -> addNewLine ed) [1..i])
+  sequence_ (map (\x -> addNewLine ed "") [1..i])
   return ed
 
-getLine :: Editor -> Int -> IO Entry
+getLine :: Editor -> Int -> IO PairBox
 getLine ed i = do
   es <- readIORef ed
   return (es !! i)
 
-getEntries :: Editor -> IO [Entry]
+getEntries :: Editor -> IO [PairBox]
 getEntries ed = do
   es <- readIORef ed
   return es
 
 -- main functions
-addLine :: Editor -> EditorView -> IO ()
-addLine ed view = do
-  l <- addNewLine ed
-  addToBox (displayBox view) l
+addLine :: Editor -> EditorView -> String -> IO ()
+addLine ed view s = do
+  l <- addNewLine ed s
+  addPairToBox (displayBox view) l
 
 removeLine :: Editor -> EditorView -> IO ()
 removeLine ed view = do
   es <- getEntries ed
   e <- removeFromEditor ed
-  (containerRemove (displayBox view) e)
+  containerRemove (displayBox view) (viewBox e)
 
 main :: IO ()
 main = do
@@ -103,11 +118,11 @@ main = do
   addLines ed 13
 
   eds <- getEntries ed
-  mapM (addToBox ebox) eds
+  mapM (addPairToBox ebox) eds
 
   view <- readIORef ev
   onClicked minusButton (removeLine ed view)
-  onClicked plusButton (addLine ed view >> widgetShowAll ebox)
+  onClicked plusButton (addLine ed view "" >> widgetShowAll ebox)
 
   onDestroy window mainQuit
   widgetShowAll window
