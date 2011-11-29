a16d8f81550bc0f2a7c22135d956ad1584910017
Martin DeMello <martindemello@gmail.com>
2011-11-29 02:37:33 -0800

    clean up the gui code a little

--- dandelion.hs
+++ dandelion.hs
@@ -6,102 +6,18 @@ import Data.Vector (Vector, (!))
 
 import Datafile
 import GuiUtils
-
--- a PairBox contains a VBox containing a Label and an Entry
-data PairBox = PairBox { pbOrig :: Label
-                       , pbText :: Entry
-                       , pbVbox :: VBox
-                       }
-
-type Editor = IORef (Vector PairBox)
-
-
-newEditor :: IO Editor
-newEditor = newIORef (V.empty)
+import Editor
+import FileIO
 
 data EditorView = EditorView { displayBox :: VBox
                              , currentLine :: Int
                              }
 
-makePairBox :: (String, String) -> IO PairBox
-makePairBox (l, s) = do
-  box <- vBoxNew False 0
-  label <- makeLabel l
-  entry <- makeEntry s
-  addToBox box label
-  addToBox box entry
-  return $ PairBox { pbOrig = label, pbText = entry, pbVbox = box }
-
-origText :: PairBox -> IO String
-origText pb = (labelGetText . pbOrig) pb
-
-newText :: PairBox -> IO String
-newText pb = (entryGetText . pbText) pb
-
-pairOfPairBox :: PairBox -> IO (String, String)
-pairOfPairBox pb = do
-  a <- origText pb
-  b <- newText pb
-  return $ (a, b)
-
-addPairToBox box v = addToBox box (pbVbox v)
-
--- editor functions
-addToEditor :: Editor -> PairBox -> IO Editor
-addToEditor ed w = do
-  es <- readIORef ed
-  writeIORef ed (es `V.snoc` w)
-  return ed
-
-removeFromEditor :: Editor -> IO PairBox
-removeFromEditor ed = do
-  es <- readIORef ed
-  e <- return $ V.last es
-  writeIORef ed (V.init es)
-  return e
-
-addNewLine :: Editor -> String -> IO PairBox
-addNewLine ed s = do
-  l <- makePairBox (s, "")
-  addToEditor ed l
-  return l
-
+-- scaffolding to test gui functionality during development
 addLines :: Editor -> Int -> IO Editor
 addLines ed i = do
   sequence_ (map (\x -> addNewLine ed (" hello " ++ (show x))) [1..i])
   return ed
-
-getLine :: Editor -> Int -> IO PairBox
-getLine ed i = do
-  es <- readIORef ed
-  return (es ! i)
-
--- just in case we change the internal representation of Editor
-getPairs :: Editor -> IO (Vector PairBox)
-getPairs ed = readIORef ed
-
-getLines :: Editor -> IO [(String, String)]
-getLines ed = do
-  es <- getPairs ed
-  ls <- V.mapM pairOfPairBox es
-  return $ V.toList ls
-
--- file <-> editor
-
-processFile :: (String -> [(String, String)]) -> Editor -> String -> IO ()
-processFile f ed path = do
-  s <- readFile path
-  ps <- mapM makePairBox (f s)
-  es <- return $ V.fromList ps
-  writeIORef ed es
-
-importFile = processFile parseImport
-loadFile   = processFile parseFile
-
-saveFile :: Editor -> String -> IO ()
-saveFile ed path = do
-  ls <- getLines ed
-  writeFile path (showFile ls)
 
 -- main functions
 addLine :: Editor -> EditorView -> String -> IO ()
